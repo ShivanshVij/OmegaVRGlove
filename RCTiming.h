0b0c3cc46ae4842559ce8f0f9e7d90befc5dce72
Unknown <s6vij@edu.uwaterloo.ca>
2017-11-19 16:56:53 -0500

    Added empty class/struct code

--- RCTiming.h
+++ RCTiming.h
@@ -1,16 +1,52 @@
 #ifndef RCTiming
 #define RCTiming
 
+#include <ugpio/ugpio.h>
+#include <iostream>
+#include <unistd.h>
+
 struct hand{
     int HAND;
     FINGER *finger;
-}
+};
 
 struct FINGER {
     int GPIOPIN;
-    
+    int IO;
+    int VALUE;
 };
 
+class RCTIMING {
+
+    public:
+        static long readvalue(FINGER& finger);
+    private:
+        bool errorcheck(FINGER& finger);
+        void setpin(FINGER& finger, int value);
+        void writepin(FINGER& finger, int value);
+        int readpin(FINGER& finger);
+        
+};
+
+bool errorcheck(FINGER& finger){
+    //Check if pin is in use
+    if(gpio_is_requested(finger.GPIOPIN) < 0){
+        cerr << "Requested pin " << finger.GPIOPIN << " is currently in use, releasing it now." << endl;
+        ugpio_free(finger.GPIOPIN);
+        return true;
+    }
+    else{
+        return true;
+    }
+}
+
+int setpin(FINGER& finger, int value){
+
+}
+
+int readpin(FINGER& finger){
+    return gpio_get_value(finger.GPIOPIN);
+}
 
 
 #endif
